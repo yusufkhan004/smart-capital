9db6f91f55aa711fdda8e7ef95a6e5327115bc93
yusuf <khanyusuf004@gmail.com>
2026-02-20 14:47:05 +0300

    project setup

---
+++ src/main/java/com/v1/SmartCapital/SmartCapitalApplication.java
@@ -0,0 +1,13 @@
+package com.v1.SmartCapital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartCapitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartCapitalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/v1/SmartCapital/SmartCapitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.v1.SmartCapital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartCapitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
